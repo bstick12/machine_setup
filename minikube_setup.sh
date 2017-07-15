41b6d85c6372b773a84464272821a549de47d7f6
Brendan Nolan <brendan.nolan@gmail.com>
2017-07-15 14:15:56 +0100

    Minikube / Kubectl setup

---
+++ minikube_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+sudo apt-get install virtualbox
+sudo apt-get install virtualbox-dkms
+sudo apt-get install linux-headers-generic
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.20.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
